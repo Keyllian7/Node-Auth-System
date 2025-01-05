0dc17cd7f4bf775fa79f0ddb635294adbef34be0
Keyllian Azevedo <157046442+Keyllian7@users.noreply.github.com>
2025-01-04 21:49:34 -0300

    Teste unitário na função de listar todos os usuários

--- tests/repository/userRepository.spec.js
+++ tests/repository/userRepository.spec.js
@@ -4,7 +4,9 @@ const userRepository = require('../../repository/userRepository');
 jest.mock('mongoose', () => ({
     model: jest.fn().mockReturnValue({
         findOne: jest.fn(),
-        find: jest.fn(),
+        find: jest.fn().mockReturnValue({
+            select: jest.fn()
+        }),
         deleteOne: jest.fn(),
         findById: jest.fn(),
         findByIdAndUpdate: jest.fn(),
@@ -25,6 +27,22 @@ describe('User Repository Tests', () => {
 
             expect(User.findOne).toHaveBeenCalledWith({ email: email });
             expect(result).toEqual(user);
+        })
+    })
+    describe('listUsers', () => {
+        it('Must list all users', async () => {
+            const users = [
+                { name: 'Jane', email: 'jane@example.com', password: '1234' },
+                { name: 'John', email: 'jhon@example.com', password: '1234' },
+                { name: 'Doe', email: 'doe@example.com', password: '1234' }
+            ];
+            User.find().select.mockResolvedValue(users);
+
+            const result = await userRepository.listUsers();
+
+            expect(User.find).toHaveBeenCalled();
+            expect(User.find().select).toHaveBeenCalledWith('-password -__v');
+            expect(result).toEqual(users);
         })
     })
 })
